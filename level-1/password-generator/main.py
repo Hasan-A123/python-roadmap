41530ee47eb0757a23fe6da3cf1c685bfb23d968
Hasan-A123 <hasanashraf13@gmail.com>
2026-07-02 17:41:47 +0100

    Create Project 7 - Password Generator structure

---
+++ level-1/password-generator/main.py
@@ -0,0 +1,13 @@
+# ====================================
+# Project 7 - Password Generator
+# Python Roadmap
+# Generate secure random passwords
+# ====================================
+
+def main():
+
+    print("=== PASSWORD GENERATOR ===")
+
+
+if __name__ == "__main__":
+    main()
