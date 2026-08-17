ec8dae357da30b39151dea874aa028b2d1b46178
Hasan-A123 <hasanashraf13@gmail.com>
2026-08-17 17:02:40 +0100

    Project 9 - Notes App Setup

---
+++ level-1/notes-app/main.py
@@ -0,0 +1,18 @@
+# ====================================
+# Project 9 - Notes App
+# Python Roadmap
+# Create, view, and save notes
+# ====================================
+
+def main():
+    print("=== NOTES APP ===")
+
+
+
+
+
+
+# Run the program only when this file
+# is executed directly.
+if __name__ == "__main__":
+    main()
